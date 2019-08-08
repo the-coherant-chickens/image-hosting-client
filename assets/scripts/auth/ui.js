23f801b538ebf6df3281bed34b7484653f5d86c2
gandreottola <gandreottola09@gmail.com>
2019-08-08 13:00:30 -0400

    Add auth for user

---
+++ assets/scripts/auth/ui.js
@@ -0,0 +1,45 @@
+'use strict'
+
+const successMessage = message => {
+  $('#message').text(message).show()
+  setTimeout(() => { $('#message').hide() }, 3000)
+  $('#message').removeClass('failure')
+  $('#message').addClass('success')
+
+  // clear out our forms
+  $('form').trigger('reset')
+}
+
+const failure = message => {
+  $('#message').text('FAIL!').show()
+  setTimeout(() => { $('#message').hide() }, 3000)
+  $('#message').removeClass('success')
+  $('#message').addClass('failure')
+
+  // clear out our forms
+  $('form').trigger('reset')
+}
+
+const signUpSuccessful = responseData => {
+  successMessage('Sign up successful!')
+}
+
+const signInSuccessful = responseData => {
+  successMessage('Sign in successful!')
+}
+
+const changePasswordSuccessful = responseData => {
+  successMessage('Password successfully changed!')
+}
+
+const signOutSuccessful = responseData => {
+  successMessage('Sign out successful!')
+}
+
+module.exports = {
+  signUpSuccessful,
+  signInSuccessful,
+  signOutSuccessful,
+  changePasswordSuccessful,
+  failure
+}
