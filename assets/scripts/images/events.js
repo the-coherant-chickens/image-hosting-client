7c732b484f90e9da8ba7e964584ec63856d3c805
JHankins09 <Hankins.JamesB@gmail.com>
2019-08-09 10:52:46 -0400

    Add additional fields for Post request

--- assets/scripts/images/events.js
+++ assets/scripts/images/events.js
@@ -1,14 +1,18 @@
 'use strict'
 
+const store = require('./../store')
 const api = require('./api')
 const ui = require('./ui')
 
 const onImageUpload = event => {
   event.preventDefault()
+  console.log('This is the form', event.target)
   const formData = new FormData(event.target)
-  // for (const [key, value] of formData.entries()) {
-  //   console.log('Yatzee!!', 'Key value is ' + key, 'Value value is ', value)
-  // }
+  formData.append('owner', store.user._id)
+  for (const [key, value] of formData.entries()) {
+    console.log('Yatzee!!', 'Key value is ' + key, 'Value value is ', value)
+  }
+  console.log(formData)
   api.imageUpload(formData)
     .then()
     .catch()
