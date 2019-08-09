f4e80056e344a4b8d72753c8c03aed98aa553b4e
JHankins09 <Hankins.JamesB@gmail.com>
2019-08-09 11:32:12 -0400

    Apply return to AJAX to provide working promise

--- assets/scripts/images/api.js
+++ assets/scripts/images/api.js
@@ -4,7 +4,7 @@ const config = require('../config')
 const store = require('../store')
 
 const imageUpload = formData => {
-  $.ajax({
+  return $.ajax({
     method: 'POST',
     data: formData,
     url: config.apiUrl + '/images',
